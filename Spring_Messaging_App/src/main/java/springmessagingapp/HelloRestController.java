8d00b9824df2951b6e67a7533766aae260640a4e
Pratikviswakarma <pratikvishwakarma1812@gmail.com>
2025-02-25 15:58:47 +0530

    [Pratik] Add. Rest call to show hello mark taylor form bridgelabz

---
+++ Spring_Messaging_App/src/main/java/springmessagingapp/HelloRestController.java
@@ -0,0 +1,32 @@
+package springmessagingapp;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloRestController {
+    // UC_1 Hello from BridgeLabz using GET request
+    @GetMapping
+    public String sayHello() {
+        return "Hello from BridgeLabz";
+    }
+
+    // UC_2 Hello with name passed as a query parameter
+    @GetMapping("/query")
+    public String sayHelloQuery(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+    // UC_3 Hello with name passed as a path variable
+    @GetMapping("/param/{name}")
+    public String sayHelloPath(@PathVariable String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+
+    // UC_4 Hello with POST request and User DTO (firstName, lastName)
+    @PostMapping("/post")
+    public String sayHelloPost(@RequestBody User user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
+    }
+
+}
